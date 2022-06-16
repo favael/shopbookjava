20b6ff516cd0c91a92d1258c2f2137999fa7622f
Rafal Dogoda <rafaldogoda86@gmail.com>
2022-06-16 15:54:05 +0200

    hej jestem Wlodek i robie zmiany

--- src/main/java/com/example/bookshop/BookShopApplication.java
+++ src/main/java/com/example/bookshop/BookShopApplication.java
@@ -8,6 +8,11 @@ public class BookShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookShopApplication.class, args);
+
+
+		System.out.println("Wlodek Branch");
+
 	}
+
 
 }
