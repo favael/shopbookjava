5a361e8f8d7194574140836e8177aecdfcdc6f96
Rafal Dogoda <rafaldogoda86@gmail.com>
2022-06-20 00:00:57 +0200

    final version with hibernate

---
+++ src/main/java/com/example/bookshop/SpringConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.bookshop;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+
+@Configuration
+class SpringConfiguration {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Bean
+    public LocalSessionFactoryBean getSessionFactory() {
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+        factoryBean.setConfigLocation(context.getResource("classpath:hibernate.cfg.xml"));
+        factoryBean.setAnnotatedClasses(Book.class);
+        return factoryBean;
+    }
+}
